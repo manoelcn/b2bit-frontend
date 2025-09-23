b8ccb75a2b10141b544af150e7b707254459b380
Manoel <candidomanoel78@gmail.com>
2025-09-23 10:55:30 -0300

    feat: create Login and Profile components

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Login: React.FC = () => {
+    return (
+        <div>
+            <h1>Página de Login</h1>
+            <p>Esta é a página de login</p>
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/pages/Profile/Profile.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Profile: React.FC = () => {
+    return (
+        <div>
+            <h1>Página de Perfil</h1>
+            <p>Esta é a página de perfil</p>
+        </div>
+    );
+};
+
+export default Profile;
