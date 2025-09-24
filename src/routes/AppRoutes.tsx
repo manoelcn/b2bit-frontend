d3350ec819a4fefc90ca9c3844f9dff6dc1cd98b
Manoel <candidomanoel78@gmail.com>
2025-09-24 10:58:12 -0300

    feat: integrate route protection

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "../pages/Login/Login";
 import Profile from "../pages/Profile/Profile";
+import ProtectedRoute from "../components/ProtectedRoute/ProtectedRoute";
+import PublicRoute from "../components/PublicRoute/PublicRoute";
 
 function AppRoutes() {
     return (
@@ -8,9 +10,9 @@ function AppRoutes() {
             <Routes>
                 <Route path="/" element={<Navigate to="/login" />} />
 
-                <Route path="/login" element={<Login />} />
+                <Route path="/login" element={<PublicRoute><Login /></PublicRoute>} />
 
-                <Route path="/profile" element={<Profile />} />
+                <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
 
                 <Route path="*" element={<div>Página não encontrada</div>} />
             </Routes>
