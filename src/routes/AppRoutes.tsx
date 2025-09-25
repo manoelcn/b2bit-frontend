674c9a9c24a3c0c40f5bf07a39c5d3527a8f1ee8
Manoel <candidomanoel78@gmail.com>
2025-09-25 18:40:51 -0300

    feat: add error not found

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -3,6 +3,7 @@ import Login from "../pages/Login/Login";
 import Profile from "../pages/Profile/Profile";
 import ProtectedRoute from "../components/ProtectedRoute/ProtectedRoute";
 import PublicRoute from "../components/PublicRoute/PublicRoute";
+import { AlertCircleIcon } from "lucide-react"
 
 function AppRoutes() {
     return (
@@ -14,7 +15,12 @@ function AppRoutes() {
 
                 <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
 
-                <Route path="*" element={<div>Página não encontrada</div>} />
+                <Route path="*" element={<div className="mt-4 rounded-md bg-red-50 p-3 border border-red-200">
+                    <div className="flex items-center gap-2">
+                        <AlertCircleIcon className="h-4 w-4 text-red-600" />
+                        <span className="text-sm font-medium text-red-800">Página não encontrada.</span>
+                    </div>
+                </div>} />
             </Routes>
         </BrowserRouter>
     );
