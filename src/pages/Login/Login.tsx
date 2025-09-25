0c4f4f8be4475ea189e29859b243e3d6778c53b0
Manoel <candidomanoel78@gmail.com>
2025-09-25 13:40:15 -0300

    feat: complete login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,6 +1,16 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { login, saveTokens } from "../../services/api";
+import {
+    Card,
+    CardContent,
+    CardFooter,
+    CardHeader,
+} from "../../components/ui/card";
+import { Button } from "../../components/ui/button"
+import { Input } from "../../components/ui/input"
+import { Label } from "../../components/ui/label"
+import { AlertCircleIcon } from "lucide-react"
 
 const Login: React.FC = () => {
     const navigate = useNavigate();
@@ -43,24 +53,52 @@ const Login: React.FC = () => {
     };
 
     return (
-        <div>
-            <h1>Login</h1>
+        <div className="w-full max-w-md">
             <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} disabled={isLoading} />
-                </div>
 
-                <div>
-                    <label htmlFor="password">Password:</label>
-                    <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} disabled={isLoading} />
-                </div>
+                <Card className="w-full max-w-sm">
+                    <CardHeader className="flex justify-center">
+                        <img src="/src/assets/b2bit.png" alt="b2bit" />
+                    </CardHeader>
+                    <CardContent>
+                        <form>
+                            <div className="flex flex-col gap-6">
+                                <div className="grid gap-2">
+                                    <Label htmlFor="email">E-mail</Label>
+                                    <Input
+                                        id="email"
+                                        type="email"
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)}
+                                        disabled={isLoading}
+                                        placeholder="@gmail.com"
+                                        required
+                                    />
+                                </div>
+                                <div className="grid gap-2">
 
-                {error && <div style={{ color: "red" }}>{error}</div>}
+                                    <Label htmlFor="password">Password</Label>
+                                    <Input id="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} disabled={isLoading} placeholder="***************" required />
+                                </div>
+                            </div>
+                        </form>
 
-                <button type="submit" disabled={isLoading}>
-                    {isLoading ? "Loading..." : "Sign In"}
-                </button>
+                        {error && (<div className="mt-4 rounded-md bg-red-50 p-3 border border-red-200">
+                            <div className="flex items-center gap-2">
+                                <AlertCircleIcon className="h-4 w-4 text-red-600" />
+                                <span className="text-sm font-medium text-red-800">{error}</span>
+                            </div>
+                        </div>)}
+
+                    </CardContent>
+                    <CardFooter className="flex-col gap-2">
+                        <Button variant="azul-b2bit" size="lg" type="submit" className="w-full" disabled={isLoading}>
+                            {isLoading ? "Loading..." : "Sign In"}
+                        </Button>
+                    </CardFooter>
+                </Card>
+
+
             </form>
         </div>
     )
